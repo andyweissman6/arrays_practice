16b6f91af23025df93fd741ecf12d932ef7eafc4
Andy Weissman <andyweissman6@gmail.com>
2023-02-22 19:23:13 -0700

    demonstrated 4 methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,19 @@
 piggies = ["fat", "round", "teacup", "chonk"]
 
 bingo_numbers = [10, 21, 32, 94]
+
+heights = [70.5, 68.4, 72.0, 65.9]
+
+test_answers = [true, false, true, false]
+
+
+print piggies.pop 
+# Returns last element in array
+
+print bingo_numbers.push(4, 2, 0)
+# Pushes elements 4, 2, and 0 to end of array
+
+print heights.shift
+# Returns an array of the first element
+
+print test_answers.unshift(false, false)
