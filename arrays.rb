383321afb18b797cfd43cb638aab003fcdbb00c5
Andy Weissman <andyweissman6@gmail.com>
2023-02-22 19:39:42 -0700

    added array methods and index positions

--- arrays.rb
+++ arrays.rb
@@ -7,7 +7,7 @@ heights = [70.5, 68.4, 72.0, 65.9]
 
 test_answers = [true, false, true, false]
 
-
+# **Array Methods**
 print piggies.pop 
 # Returns last element in array
 
@@ -18,3 +18,11 @@ print heights.shift
 # Returns an array of the first element
 
 print test_answers.unshift(false, false)
+# Sends the elements in parentheses to front of array, pushing others back
+
+# **index positions**
+print piggies[2]
+# this would print "teacup" as most programming languages denote the first element in an array as "0."
+print bingo_numbers[3]
+# in this example, we would expect "94" to be returned.
+# It should be noted that once array methods are uses, the index positions for all the elements will change from their previous position in the array
