bb2b0e54f44094a25cee5485577960d4175a0791
Andy Weissman <andyweissman6@gmail.com>
2023-02-23 08:01:20 -0700

    changed all puts to p

--- arrays.rb
+++ arrays.rb
@@ -9,7 +9,7 @@ test_answers = [true, false, true, false]
 
 # **Array Methods**
 puts piggies.pop 
-# Returns last element in array
+# Returns last element in array, and also removes it from the array.
 p piggies
 
 
@@ -17,20 +17,20 @@ p bingo_numbers.push(4, 2, 0)
 # Pushes elements 4, 2, and 0 to end of array
 
 
-puts heights.shift
+p heights.shift
 # Returns an array of the first element
 
-puts test_answers.unshift(false, false)
+p test_answers.unshift(false, false)
 # Sends the elements in parentheses to front of array, pushing others back
 
 # **index positions**
-print piggies[2]
+p piggies[2]
 # this would print "teacup" as most programming languages denote the first element in an array as "0."
-print bingo_numbers[3]
+p bingo_numbers[3]
 # in this example, we would expect "94" to be returned.
 # It should be noted that once array methods are uses, the index positions for all the elements will change from their previous position in the array
 
 # **Final array method**
-print heights.clear
+p heights.clear
 # this would completely clear the array
 
