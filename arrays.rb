9e8bac955443cf4bc4e66d3f5b87181809d9d367
Andy Weissman <andyweissman6@gmail.com>
2023-02-23 07:52:16 -0700

    changed print to puts and p

--- arrays.rb
+++ arrays.rb
@@ -8,16 +8,19 @@ heights = [70.5, 68.4, 72.0, 65.9]
 test_answers = [true, false, true, false]
 
 # **Array Methods**
-print piggies.pop 
+puts piggies.pop 
 # Returns last element in array
+p piggies
 
-print bingo_numbers.push(4, 2, 0)
+
+p bingo_numbers.push(4, 2, 0)
 # Pushes elements 4, 2, and 0 to end of array
 
-print heights.shift
+
+puts heights.shift
 # Returns an array of the first element
 
-print test_answers.unshift(false, false)
+puts test_answers.unshift(false, false)
 # Sends the elements in parentheses to front of array, pushing others back
 
 # **index positions**
